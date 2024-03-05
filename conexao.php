40d8c626f4ba2b1e32198135b885df55884ab72a
Bruno Imanaka <bn.imanaka@@gmail.com>
2024-03-04 23:55:16 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'tito';
+    
+    $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
